9468d04ebfb06f1184c01a5f9dd1f122acb5a4a4
Jessica <duellje@oregonstate.edu>
2020-08-19 12:24:15 -0400

    added a test

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,11 @@
-const { add } = require('./calculator.js');
+const { add } = require("./calculator.js");
 
 // test away!
+describe("calculator function", () => {
+  it("adds two numbers together", () => {
+    const expected = 4;
+    const actual = add(2, 2);
+    expect(actual).not.toBe(5);
+    expect(actual).toBe(expected);
+  });
+});
