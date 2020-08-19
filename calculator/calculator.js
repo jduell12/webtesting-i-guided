a9a11d4f9364f66ce7b68bf0a8d5add5eb902030
Jessica <duellje@oregonstate.edu>
2020-08-19 13:10:55 -0400

    made more tests

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,7 +1,12 @@
 module.exports = {
   add,
+  multiply,
 };
 
-function add(a, b) {
+function add(a, b = 0) {
   return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
 }
--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,33 +1,39 @@
-const { add } = require("./calculator.js");
-const { TestScheduler } = require("jest");
+const { add, multiply } = require("./calculator.js");
 
 //triple a thinking model
 //arrange - setup for the test
 //act - execute the code you're testing
 //assert - test the assumption on how the code should work
 
-test("adds two numbers correctly", () => {
-  //arrange
-  const a = 2;
-  const b = 2;
-  const sum = 4;
+describe("add()", () => {
+  test("adds two numbers correctly", () => {
+    //arrange
+    const a = 2;
+    const b = 2;
+    const sum = 4;
 
-  //act
-  const result = add(a, b);
+    //act
+    const result = add(a, b);
 
-  //assert
-  expect(result).toBe(sum);
+    //assert
+    expect(result).toBe(sum);
+  });
+
+  it("should add two numbers (using it)", () => {
+    expect(add(2, 2)).toBe(4);
+    expect(add(0, 0)).toBe(0);
+    expect(add(2, -2)).toBe(0);
+  });
+
+  it("should return the value if called with a single value", () => {
+    expect(add(2)).toBe(2);
+    expect(add(0)).toBe(0);
+    expect(add(-1)).toBe(-1);
+  });
 });
 
-it("should add two numbers (using it)", () => {
-  expect(add(2, 2)).toBe(4);
+describe("multiply()", () => {
+  it("multiples two numbers correctly", () => {
+    expect(multiply(2, 1)).toBe(2);
+  });
 });
-
-// describe("calculator function", () => {
-//   it("adds two numbers together", () => {
-//     const expected = 4;
-//     const actual = add(2, 2);
-//     expect(actual).not.toBe(5);
-//     expect(actual).toBe(expected);
-//   });
-// });
